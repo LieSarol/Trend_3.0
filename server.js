5fdd4dc769dbd936fed2f9cc4aabc2f24b31bdb2
Cubby <162946353+LieSarol@users.noreply.github.com>
2025-08-12 19:36:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const feeds = [
   { name: "TechCrunch", url: "https://techcrunch.com/feed/", type: "news" },
   { name: "The Verge", url: "https://www.theverge.com/rss/index.xml", type: "news" },
   { name: "Smashing Magazine", url: "https://www.smashingmagazine.com/feed/", type: "blogs" },
-  { name: "BuzzFeed", url: "https://www.buzzfeed.com/index.xml", type: "blogs" }  // swapped here
+  { name: "Bing Tech News", url: "https://www.bing.com/news/search?q=technology&format=rss", type: "news" }
 ];
 
 // Timestamped logger
