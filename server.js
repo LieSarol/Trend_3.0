b085cfd5138648893f32d405bd219fc9c35977c1
Cubby <162946353+LieSarol@users.noreply.github.com>
2025-08-17 08:03:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const feeds = [
   { name: "TechCrunch", url: "https://techcrunch.com/feed/", type: "news" },
   { name: "The Verge", url: "https://www.theverge.com/rss/index.xml", type: "news" },
   { name: "Smashing Magazine", url: "https://www.smashingmagazine.com/feed/", type: "blogs" },
-  { name: "CNN News", url: "http://rss.cnn.com/rss/cnn_topstories.rss", type: "news" }
+  { name: "CNN News", url: "http://rss.cnn.com/rss/cnn_topstories.rss", type: "news" },
   { name: "Minecraft News", url: "https://news.google.com/rss/search?q=Minecraft", type: "news" }
 ];
 
